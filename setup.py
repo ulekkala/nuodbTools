a456fa584dad8f28e29dc63bee4b8ae0d8d733ce
Rich Kourtz <rkourtz@nuodb.com>
2014-03-07 17:06:42 -0500

    Validate Python version

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ if v[0] == 2 and v[1] == 7:
         scripts=["nuodb_backup.py", "nuodb_cluster.py", "nuodb_load.py", "nuodb_tarball_installer.py"],
         zip_safe=False)
 else:
-  print "The nuodbTools module and some of its dependencies only work on Python version 2.7. Detected %s. Cannot continue" % ".".join(v[0:2])
+  print "The nuodbTools module and some of its dependencies only work on Python version 2.7. Detected %s. Cannot continue." % ".".join(str(e) for e in v[0:2])
   exit(2)
