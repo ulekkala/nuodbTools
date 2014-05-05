165ca4056734ab179a4dd3c3e6fd2eef8466530b
Rich Kourtz <rkourtz@nuodb.com>
2014-05-05 14:54:03 -0400

    Force yum -y update due to buggy curl version

--- nuodbTools/cluster/templates/init.py
+++ nuodbTools/cluster/templates/init.py
@@ -4,6 +4,7 @@ import json, os, urllib2
 
 commands = [
             "hostname $hostname",
+            "yum -y update",
             "yum -y install git",
             "yum -y install https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-11.8.2-1.el6.x86_64.rpm",
             "mkdir -p /var/chef/cookbooks",
